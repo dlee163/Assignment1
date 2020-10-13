62f6c53b7e5f6b9e3b5829c4d969db87eb235483
dlee163 <71297960+dlee163@users.noreply.github.com>
2020-10-12 20:05:53 -0700

    Add files via upload

--- AddingMachine.java
+++ AddingMachine.java
@@ -2,25 +2,32 @@
 
 public class AddingMachine {
   private int total;
+  private String A = "0";
   
   public AddingMachine () {
-    total = 0;  // not needed - included for clarity
+    total = 0; 
   }
   
   public int getTotal () {
-    return 0;
+    return total;
   }
   
   public void add (int value) {
+      total = total + value;
+      A = A + "+" + value;
   }
 
   public void subtract (int value) {
+       total = total - value;
+      A = A + "-" + value;
   }
 
   public String toString () {
-    return "";
+    return A;
   }
 
   public void clear() {
+      total = 0;
+      A = "0";
   }
 }
